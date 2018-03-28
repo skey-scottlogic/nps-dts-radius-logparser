02a567b237c951646fffffc3062d1e79c69932d2
Simon Key <skey@scottlogic.co.uk>
2018-03-28 11:12:18 +0100

    Updated and customised.

--- README.md
+++ README.md
@@ -1,46 +1,33 @@
 Parser for MS NPS dts/xml RADIUS log files
 ==========================================
 
-This is just a quick&dirty parser, because I was tired of reading DTS XML log files while troubleshooting a RADIUS login issue. All the parser does at the moment is translating reason codes, packet types and returning powershell objects for every log entry. This allows you to use ?/where in the pipe to search for specific patterns
+Simple parser for NPS log files.  Based on the work of Jochen Bartl <jochen.bartl@gmail.com>, from https://github.com/verbosemode/nps-dts-radius-logparser.
 
-It is a PowerShell script, because that's what you get on most Windows 2008 R2 servers by default.
+Parameters
+----------
 
-Feel free to send me complaints/patches/improvements/ideas ;-)
-
-
-Known issues
-------------
-
-* Damn slow
-* Reads only one file from command line
-
-
-TODO
-----
-
-* Read input from pipe
-* Command line parameters for basic filtering/searching
-...
-
+* log - log file to read (defaults to the newest iaslog*.log in C:\Windows\System32\LogFiles\)
+* tail - number of lines to read (defaults to 100)
+* follow - boolean, follow the log file
 
 Usage Example
 -------------
 
-	PS C:\Windows\System32\LogFiles\NPS> .\nps-dts-radius-logparser.ps1 .\iaslog47.log | ? { $_.ClientFriendlyName -eq "NET_NCS" -and $_.SamAccountName -like "*manbearpig*" }
-	<output omitted>
-	Timestamp          : 04/19/2012 10:05:21.579
-	ComputerName       : srvdc001
-	NpPolicyName       : NP_NET_NCS
-	ProxyPolicyName    : CRP_NET_NCS
-	EventSource        : IAS
-	UserName           :
-	ClientIpAddress    : 192.0.2.10
-	ClientVendor       : 0
-	ClientFriendlyName : NET_NCS
-	SamAccountName     : SOUTHPARK\manbearpig
-	PacketType         : 2
-	PacketTypeName     : Access-Accept
-	ReasonCode         : 0
-	ReasonCodeName     : IAS_SUCCESS
-	<output omitted>
+	PS > .\nps-dts-radius-logparser.ps1 | ft
+	
+	Timestamp               NPPolicyName ProxyPolicyName UserName                 ClientFriendlyName SAMAccountName   PacketTypeName ReasonCodeName
+	---------               ------------ --------------- --------                 ------------------ --------------   -------------- --------------
+	...
+	03/28/2018 08:46:11.175 LAN          LAN             host/WS01043.example.com Wifi: wifi-f       EXAMPLE\WS01043$ Access-Request IAS_SUCCESS
+	03/28/2018 08:46:11.175 LAN          LAN                                      Wifi: wifi-f       EXAMPLE\WS01043$                IAS_SUCCESS
+	03/28/2018 08:46:11.769 Wired        Wired           host/WS00882.example.com Switch: sw-a       EXAMPLE\WS00882$ Access-Request IAS_SUCCESS
+	03/28/2018 08:46:11.769 Wired        Wired                                    Switch: sw-a       EXAMPLE\WS00882$                IAS_SUCCESS
+	03/28/2018 08:46:12.769 Wired        Wired           host/WS00123.example.com Switch: sw-a       EXAMPLE\WS00123$ Access-Request IAS_SUCCESS
+	03/28/2018 08:46:12.769 Wired        Wired                                    Switch: sw-a       EXAMPLE\WS00123$                IAS_SUCCESS
 
+	PS > .\nps-dts-radius-logparser.ps1 -follow | ? { $_.UserName -eq "host/WS00882.example.com" -and $_.ClientFriendlyName -like "Switch*" } | ft
+
+	Timestamp               NPPolicyName ProxyPolicyName UserName                 ClientFriendlyName SAMAccountName   PacketTypeName ReasonCodeName
+	---------               ------------ --------------- --------                 ------------------ --------------   -------------- --------------
+	03/28/2018 08:46:11.769 Wired        Wired           host/WS00882.example.com Switch: sw-a       EXAMPLE\WS00882$ Access-Request IAS_SUCCESS
+	03/28/2018 08:46:11.769 Wired        Wired                                    Switch: sw-a       EXAMPLE\WS00882$                IAS_SUCCESS
--- nps-dts-radius-logparser.ps1
+++ nps-dts-radius-logparser.ps1
@@ -1,73 +1,77 @@
 # Log parser for MS NPS dts RADIUS logs
-# v 0.0.1 / 20120412
-# Author: Jochen Bartl <jochen.bartl@gmail.com>
+# v 0.0.2 / 20180328
+# Author: Simon Key <skey@scottlogic.com>
 
-$filename = $args[0]
+param (
+    [string]$log = "C:\Windows\System32\LogFiles\" + (Get-ChildItem -Path "C:\Windows\System32\LogFiles" -File iaslog*.log | Sort-Object LastWriteTime -Descending | Select-Object -First 1),
+    [int]$tail = 100,
+    [switch]$follow = $false
+)
 
-$PACKET_TYPES = @{
-	1 = "Access-Request";
-	2 = "Access-Accept";
-	3 = "Access-Reject";
-	4  = "Accounting-Request"
+$packetTypes = @{
+    1 = "Access-Request";
+    2 = "Access-Accept";
+    3 = "Access-Reject";
+    4 = "Accounting-Request";
 }
 
-$REASON_CODES = @{
-	0 = "IAS_SUCCESS";
-	1 = "IAS_INTERNAL_ERROR";
-	2 = "IAS_ACCESS_DENIED";
-	3 = "IAS_MALFORMED_REQUEST";
-	4 = "IAS_GLOBAL_CATALOG_UNAVAILABLE";
-	5 = "IAS_DOMAIN_UNAVAILABLE";
-	6 = "IAS_SERVER_UNAVAILABLE";
-	7 = "IAS_NO_SUCH_DOMAIN";
-	8 = "IAS_NO_SUCH_USER";
-	16 = "IAS_AUTH_FAILURE";
-	17 = "IAS_CHANGE_PASSWORD_FAILURE";
-	18 = "IAS_UNSUPPORTED_AUTH_TYPE";
-	32 = "IAS_LOCAL_USERS_ONLY";
-	33 = "IAS_PASSWORD_MUST_CHANGE";
-	34 = "IAS_ACCOUNT_DISABLED";
-	35 = "IAS_ACCOUNT_EXPIRED";
-	36 = "IAS_ACCOUNT_LOCKED_OUT";
-	37 = "IAS_INVALID_LOGON_HOURS";
-	38 = "IAS_ACCOUNT_RESTRICTION";
-	48 = "IAS_NO_POLICY_MATCH";
-	64 = "IAS_DIALIN_LOCKED_OUT";
-	65 = "IAS_DIALIN_DISABLED";
-	66 = "IAS_INVALID_AUTH_TYPE";
-	67 = "IAS_INVALID_CALLING_STATION";
-	68 = "IAS_INVALID_DIALIN_HOURS";
-	69 = "IAS_INVALID_CALLED_STATION";
-	70 = "IAS_INVALID_PORT_TYPE";
-	71 = "IAS_INVALID_RESTRICTION";
-	80 = "IAS_NO_RECORD";
-	96 = "IAS_SESSION_TIMEOUT";
-	97 = "IAS_UNEXPECTED_REQUEST";
+$reasonCodes = @{
+    0 = "IAS_SUCCESS";
+    1 = "IAS_INTERNAL_ERROR";
+    2 = "IAS_ACCESS_DENIED";
+    3 = "IAS_MALFORMED_REQUEST";
+    4 = "IAS_GLOBAL_CATALOG_UNAVAILABLE";
+    5 = "IAS_DOMAIN_UNAVAILABLE";
+    6 = "IAS_SERVER_UNAVAILABLE";
+    7 = "IAS_NO_SUCH_DOMAIN";
+    8 = "IAS_NO_SUCH_USER";
+    16 = "IAS_AUTH_FAILURE";
+    17 = "IAS_CHANGE_PASSWORD_FAILURE";
+    18 = "IAS_UNSUPPORTED_AUTH_TYPE";
+    32 = "IAS_LOCAL_USERS_ONLY";
+    33 = "IAS_PASSWORD_MUST_CHANGE";
+    34 = "IAS_ACCOUNT_DISABLED";
+    35 = "IAS_ACCOUNT_EXPIRED";
+    36 = "IAS_ACCOUNT_LOCKED_OUT";
+    37 = "IAS_INVALID_LOGON_HOURS";
+    38 = "IAS_ACCOUNT_RESTRICTION";
+    48 = "IAS_NO_POLICY_MATCH";
+    64 = "IAS_DIALIN_LOCKED_OUT";
+    65 = "IAS_DIALIN_DISABLED";
+    66 = "IAS_INVALID_AUTH_TYPE";
+    67 = "IAS_INVALID_CALLING_STATION";
+    68 = "IAS_INVALID_DIALIN_HOURS";
+    69 = "IAS_INVALID_CALLED_STATION";
+    70 = "IAS_INVALID_PORT_TYPE";
+    71 = "IAS_INVALID_RESTRICTION";
+    80 = "IAS_NO_RECORD";
+    96 = "IAS_SESSION_TIMEOUT";
+    97 = "IAS_UNEXPECTED_REQUEST";
 }
 
+[string[]]$DefaultProperties = "Timestamp","NPPolicyName","ProxyPolicyName","UserName","ClientFriendlyName","SAMAccountName","PacketTypeName","ReasonCodeName"
+$ddps = New-Object System.Management.Automation.PSPropertySet DefaultDisplayPropertySet, $DefaultProperties
+$PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]$ddps
 
-foreach ($line in gc $filename) {
-	$logline = [xml]$line
-	$logline = $logline.Event
-	
-	$logobj = New-Object PSObject
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "Timestamp" -value $logline.Timestamp."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ComputerName" -value $logline."Computer-Name"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "NpPolicyName" -value $logline."NP-Policy-Name"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ProxyPolicyName" -value $logline."Proxy-Policy-Name"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "EventSource" -value $logline."Event-Source"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "UserName" -value $logline."User-Name"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ClientIpAddress" -value $logline."Client-IP-Address"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ClientVendor" -value $logline."Client-Vendor"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ClientFriendlyName" -value $logline."Client-Friendly-Name"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "SamAccountName" -value $logline."SAM-Account-Name"."#text"
-	
-	
-	
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "PacketType" -value $logline."Packet-Type"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "PacketTypeName" -value $PACKET_TYPES[[int]$logline."Packet-Type"."#text"]
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ReasonCode" -value $logline."Reason-Code"."#text"
-	Add-Member -InputObject $logobj -MemberType NoteProperty -name "ReasonCodeName" -value $REASON_CODES[[int]$logline."Reason-Code"."#text"]
-	
-	$logobj
+Function line ($logline) {
+    $l = [pscustomobject]@{
+        Timestamp           = $logline.Timestamp."#text"
+        ComputerName        = $logline."Computer-Name"."#text"
+        NPPolicyName        = $logline."NP-Policy-Name"."#text"
+        ProxyPolicyName     = $logline."Proxy-Policy-Name"."#text"
+        EventSource         = $logline."Event-Source"."#text"
+        UserName            = $logline."User-Name"."#text"
+        ClientIPAddress     = $logline."Client-IP-Address"."#text"
+        ClientVendor        = $logline."Client-Vendor"."#text"
+        ClientFriendlyName  = $logline."Client-Friendly-Name"."#text"
+        SAMAccountName      = $logline."SAM-Account-Name"."#text"
+        PacketType          = $logline."Packet-Type"."#text"
+        PacketTypeName      = $packetTypes[[int]$logline."Packet-Type"."#text"]
+        ReasonCode          = $logline."Reason-Code"."#text"
+        ReasonCodeName      = $reasonCodes[[int]$logline."Reason-Code"."#text"]
+    }
+    Add-Member -InputObject $l -MemberType MemberSet -Name PSStandardMembers -Value $PSStandardMembers
+    return $l
 }
+
+Get-Content $log -Tail $tail -Wait:$follow | % { line(([xml]$_).Event) }
